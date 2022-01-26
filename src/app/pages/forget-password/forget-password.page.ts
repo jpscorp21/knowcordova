ac363c390bf7a16d0eb341a3b7091acef6c2e0e1
jpscorp21 <jpscorp21@gmail.com>
2022-01-26 09:17:24 -0300

    Codigo verificacion terminado

--- src/app/pages/forget-password/forget-password.page.ts
+++ src/app/pages/forget-password/forget-password.page.ts
@@ -1,27 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {BehaviorSubject} from "rxjs";
-import {Router} from "@angular/router";
-import {PerfilService} from "../../services/perfil.service";
-import {AuthService} from "../../services/auth.service";
-import {ToastService} from "../../services/shared/toast.service";
-import {PersonaLogin} from "../../interfaces/interface";
+/* eslint-disable @typescript-eslint/naming-convention */
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {BehaviorSubject} from 'rxjs';
+import {Router} from '@angular/router';
+import {PerfilService} from '../../services/perfil.service';
+import {AuthService} from '../../services/auth.service';
+import {ToastService} from '../../services/shared/toast.service';
+import {PersonaLogin} from '../../interfaces/interface';
+import {AlertService} from '../../services/shared/alert.service';
 
 @Component({
   selector: 'app-forget-password',
   templateUrl: './forget-password.page.html',
   styleUrls: ['./forget-password.page.scss'],
 })
-export class ForgetPasswordPage implements OnInit {
+export class ForgetPasswordPage implements OnInit, AfterViewInit {
 
   public ngForm: FormGroup;
   confirmarClicked = new BehaviorSubject(false);
+  codigo$ = new BehaviorSubject('');
 
   constructor(
     public formBuilder: FormBuilder,
     public router: Router,
     public perfil: PerfilService,
     private readonly auth: AuthService,
+    private readonly alert: AlertService,
     private toast: ToastService,
   ) {
     this.createForm();
@@ -50,7 +54,7 @@ export class ForgetPasswordPage implements OnInit {
       const dataForSave: PersonaLogin = {
         email: this.ngForm.value.email,
         password: this.ngForm.value.password,
-      }
+      };
 
       const data = await this.auth.forgetPassword(dataForSave);
       this.confirmarClicked.next(false);
@@ -74,9 +78,12 @@ export class ForgetPasswordPage implements OnInit {
     this.ngForm = this.formBuilder.group({
       email: ['', Validators.email],
       password: ['', Validators.required],
+      // eslint-disable-next-line @typescript-eslint/naming-convention
       password_confirmation: ['', Validators.required],
+      codigo: [''],
+      codigo_verificacion: ['123456']
     }, {
-      validator: this.passwordMatchValidator
+      validator: [this.passwordMatchValidator, this.codigoMatchValidator]
     });
   }
 
@@ -86,8 +93,33 @@ export class ForgetPasswordPage implements OnInit {
 
     if (password !== confirmPassword) {
 
+      // eslint-disable-next-line @typescript-eslint/naming-convention
       control.get('password_confirmation').setErrors({NoPassswordMatch: true});
     }
   }
 
+  codigoMatchValidator(control: AbstractControl) {
+    const codigo: string = control.get('codigo').value;
+    const codigo_verificacion: string = control.get('codigo_verificacion').value;
+
+    if (codigo !== codigo_verificacion) {
+
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      control.get('codigo').setErrors({NoCodigoMatch: true});
+    }
+  }
+
+  async fetchCodigo() {
+    if (this.ngForm.value.password !== this.ngForm.value.password_confirmation) {
+      return;
+    }
+
+    const data = await this.auth.verificacionEmail({email: this.ngForm.value.email});
+
+    if (data && data.dsc && data.codigo) {
+      this.ngForm.patchValue({codigo_verificacion: data.codigo.toString()})
+
+      await this.alert.alertMessage('Un código se ha enviado a su correo. ');
+    }
+  }
 }
